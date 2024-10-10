71d480482f8fb0159d53b2e8255af291ab98d40f
KimBaek-Seyeong <ksya2186@gmail.com>
2024-10-10 17:24:40 +0900

    .

--- Core/RecordStorage.cs
+++ Core/RecordStorage.cs
@@ -6,10 +6,38 @@ namespace Core
     public class RecordStorage : IRecordStorage
     {
         #region Variable
+        readonly IBlockStorage storage;
+
+        const int MaxRecordSize = 4194304; // 4MB
+        cosnt int kNextBlockId = 0;
+        const int kRecordLength = 1;
+        const int kBlockContentLength = 2;
+        const int kPreviousBlockId = 3;
+        const int kIsDeleted = 4;
+        /*
+        * The common interpretation of the prefix 'k' is 'konstant' (constant number).
+        * In the early days of C++ development, a naming convention called Hungarian notation was widely used.
+        * At that time, people began using the prefix 'k' to denote constant numbers.
+        * The exact reason for choosing 'k' instead of 'c' (for 'constant') is not known,
+        * but most developers belive it comes from the German word 'konstant', which means 'constant'.
+        */
         #endregion
 
         #region Constructor
-        public RecordStorage() { }
+        public RecordStorage(IBlockStorage storage)
+        {
+            if (storage == null)
+            {
+                throw new ArgumentNullException("storage");
+            }
+
+            this.storage = storage;
+
+            if (storage.BlockHeaderSize < 48)
+            {
+                throw new ArgumentException("Record storage needs at least 48 header bytes");
+            }
+        }
         #endregion
 
         #region Event Command
@@ -20,7 +48,29 @@ namespace Core
         public void Update(uint recordId, byte[] data) { }
 
         // Grab a record's data
-        public byte[] Find(uint recordId) { }
+        public virtual byte[] Find(uint recordId)
+        {
+            // first grab the block
+            using (var block = storage.Find(recordId))
+            {
+                if (block == null)
+                {
+                    return null;
+                }
+L
+                // if this is a deleted block then ignore it
+                if (1L == block.GetHeader(kIsDeleted))
+                { 
+                    return null;
+                }
+
+                // if this block is a child block then also ignore it
+                if (0L != block.GetHeader (kPreviousBlockId))
+                {
+                    return null;
+                }
+            }
+        }
 
         // This creates new empty record
         public uint Create() { }
